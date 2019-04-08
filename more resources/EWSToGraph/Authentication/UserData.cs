399af2e120a73602222b291b337da9879fdc0d40
qiang <1043360492@qq.com>
2019-04-08 16:27:47 +0800

    fix EWS errors

---
+++ more resources/EWSToGraph/Authentication/UserData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Exchange.WebServices.Data;
+
+namespace ExchangeAuthentication
+{
+    public interface IUserData
+    {
+        ExchangeVersion Version { get; }
+        string EmailAddress { get; set; }
+        string Password { get; set; }
+        Uri AutodiscoverUrl { get; set; }
+    }
+
+    public class UserDataFromConsole : IUserData
+    {
+        public static UserDataFromConsole UserData;
+
+        public static IUserData GetUserData()
+        {
+            return UserData;
+        }
+        public ExchangeVersion Version { get { return ExchangeVersion.Exchange2016; } }
+
+        public string EmailAddress
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        public Uri AutodiscoverUrl
+        {
+            get;
+            set;
+        }
+    }
+}
